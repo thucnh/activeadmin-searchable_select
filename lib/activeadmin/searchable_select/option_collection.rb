c0f6a8e171bfc4fbfe7ba38680be6e329e9f48e4
thucnh <thucnh@gmail.com>
2019-05-19 17:16:00 +0700

    Update option_collection.rb
    allow to attache other attributes

--- lib/activeadmin/searchable_select/option_collection.rb
+++ lib/activeadmin/searchable_select/option_collection.rb
@@ -6,6 +6,7 @@ module ActiveAdmin
         @name = name
         @scope = extract_scope_option(options)
         @display_text = extract_display_text_option(options)
+        @attached_attributes = extract_attached_attributes_option(options)
         @filter = extract_filter_option(options)
         @per_page = options.fetch(:per_page, 10)
       end
@@ -27,6 +28,10 @@ module ActiveAdmin
         @display_text.call(record)
       end
 
+      def attached_attributes(record) 
+        @attached_attributes.call(record)
+      end
+
       def collection_action_name
         "#{@name}_options"
       end
@@ -37,7 +42,8 @@ module ActiveAdmin
         results = records.map do |record|
           {
             id: record.id,
-            text: display_text(record)
+            text: display_text(record),
+            attributes: attached_attributes(record)
           }
         end
 
@@ -84,6 +90,15 @@ module ActiveAdmin
           end
 
           ->(record) { record.send(text_attribute) }
+        end
+      end
+
+      def extract_attached_attributes_option(options)
+        attached_attributes = (options.fetch(:attached_attributes) do
+          return ->(record) { {} }
+        end).map { |attribute_name| attribute_name.to_s }
+        ->(record) do 
+          record.attributes.slice(*attached_attributes) 
         end
       end
 
